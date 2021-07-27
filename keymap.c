33d22a644096bd7b2627bc194ac142ff7ab026e9
David Sunshine <david@sunshines.org>
2021-07-27 15:36:56 -0400

    Space Cadet Enter
    The thumb row is not fully defined but everything is now usable. The
    Reset on the LOWER layer does not seem to allow for a reflash.

--- keymap.c
+++ keymap.c
@@ -23,23 +23,19 @@ enum layer_names {
 };
 
 enum custom_keycodes {
-    QWERTY = SAFE_RANGE,
-    LOWER,
-    RAISE,
-    ADJUST
+    OP_ARROW = SAFE_RANGE,
 };
 
-#define CTL_TAB CTL_T(KC_TAB)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_split_3x7_4(
-        CTL_TAB,         KC_Q,   KC_W,   KC_F,   KC_P,   KC_B,    KC_LPRN,      KC_RPRN,    KC_J     , KC_L   , KC_U   , KC_Y   , KC_SLASH , KC_EQUAL,
+        KC_TAB,         KC_Q,   KC_W,   KC_F,   KC_P,   KC_B,    KC_LPRN,      KC_RPRN,    KC_J     , KC_L   , KC_U   , KC_Y   , KC_SLASH , KC_EQUAL,
         KC_GESC,         KC_A,   KC_R,   KC_S,   KC_T,   KC_G,    KC_LBRC,      KC_RBRC,    KC_K     , KC_N   , KC_E   , KC_I   , KC_O     , KC_QUOT,
         LCTL_T(KC_BSLS), KC_Z,   KC_X,   KC_C,   KC_D,   KC_V,    MO(_LOWER),   MO(_RAISE),  KC_M     , KC_H   , KC_COMM, KC_DOT , KC_SCOLON, KC_MINS,
-                                    KC_LGUI, LOWER  , KC_BSPACE, KC_BSPACE,     KC_DELETE, KC_SPACE, RAISE  , KC_RALT
+                                    KC_LGUI, XXXXXXX, KC_BSPACE, KC_DELETE,     KC_SFTENT, KC_SPACE, XXXXXXX, KC_RALT
     ),
     [_LOWER] = LAYOUT_split_3x7_4(
-        _______, KC_AT,   _______,   KC_UP,  _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
+        _______, KC_AT,   _______,   KC_UP,  _______, _______, _______,    OP_ARROW, _______, _______, _______, _______, _______, RESET,
         _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
         _______, _______,  _______,  _______,  _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
                                   _______,  _______, _______, _______,    _______, _______, _______, _______
@@ -54,7 +50,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
-                                _______, _______, _______, _______,    _______, _______, _______, _______
+                                   _______, _______, _______, _______,    _______, _______, _______, _______
     )
 };
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case OP_ARROW:
+            if (record->event.pressed) {
+                // When OP_ARROW is pressed
+                SEND_STRING("->");
+            } else {
+                // When OP_ARROW is released
+            }
+            break;
+    }
+    return true; // continue normal processing (we did not replace or alter any functionality)
+}
 
