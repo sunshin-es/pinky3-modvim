3ee28d8aa4e5bbada09702482729efbb4bcb0653
David Sunshine <david@sunshines.org>
2021-08-19 09:56:10 -0400

    Remove F8 to F10 from raise layer
    These have been removed and made transparent so the comma and period are
    still available when typing numbers.

--- keymap.c
+++ keymap.c
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_RAISE] = LAYOUT_split_3x7_4(
         _______, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, _______,    _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
         _______, KC_1,     KC_2,   KC_3,   KC_4,   KC_5, _______,    _______,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0, _______,
-        _______, KC_F1,   KC_F2,  KC_F3,  KC_F4,  KC_F5, _______,    _______,  KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10, _______,
+        _______, KC_F1,   KC_F2,  KC_F3,  KC_F4,  KC_F5, _______,    _______,  KC_F6,  KC_F7,  _______,  _______, _______, _______,
                                 _______, _______, _______, _______,    _______, _______, _______, _______
     ),
     [_ADJUST] = LAYOUT_split_3x7_4(
