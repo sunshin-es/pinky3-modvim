d6c593e14e7816c2e2c6e4ed36cc52c6fe3b1168
David Sunshine <david@sunshines.org>
2021-08-23 15:15:51 -0400

    Tap Dance for ESC
    1. Single press sends ESC
    2. Double press sends ESC followed by ":"

--- keymap.c
+++ keymap.c
@@ -50,13 +50,27 @@ enum custom_keycodes {
 /* Tap dance declarations */
 enum {
     TD_BSPACE,
+    TD_ESC,
 };
 
 // Tap Dance definitions
 qk_tap_dance_action_t tap_dance_actions[] = {
     // Tap once for backspace, twice for ctrl + backspace
     [TD_BSPACE] = ACTION_TAP_DANCE_DOUBLE(KC_BSPACE, LCTL(KC_BSPACE)),
+    [TD_ESC] = ACTION_TAP_DANCE_FN(td_esc),
 };
+
+// Tap Dance functions
+void td_esc(qk_tap_dance_state_t *state, void *user_data) {
+    if (state->count == 1) {
+        SEND_STRING(SS_TAP(X_ESC));
+        reset_tap_dance(state);
+    }
+    else if (state->count == 2) {
+        SEND_STRING(SS_TAP(X_ESC) SS_TAP(X_COLN));
+    }
+}
+
 
 /* Give the space cadet enter a new name since we will tweak it to work with
  * space */
@@ -65,9 +79,9 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_split_3x7_4(
         KC_TAB,          KC_Q,   KC_W,   KC_F,   KC_P,   KC_B,    KC_LPRN,      KC_RPRN,    KC_J     , KC_L   , KC_U   , KC_Y   , KC_SLASH , KC_EQUAL,
-        KC_GESC,         KC_A,   KC_R,   KC_S,   KC_T,   KC_G,    KC_LBRC,      KC_RBRC,    KC_K     , KC_N   , KC_E   , KC_I   , KC_O     , KC_QUOT,
+        TD(TD_ESC),      KC_A,   KC_R,   KC_S,   KC_T,   KC_G,    KC_LBRC,      KC_RBRC,    KC_K     , KC_N   , KC_E   , KC_I   , KC_O     , KC_QUOT,
         LCTL_T(KC_BSLS), KC_Z,   KC_X,   KC_C,   KC_D,   KC_V,    MO(_LOWER),   MO(_RAISE),  KC_M     , KC_H   , KC_COMM, KC_DOT , KC_SCOLON, KC_MINS,
-                                    KC_LGUI, MO(_ADJUST), TD(TD_BSPACE), KC_DELETE,     KC_ENT, KC_SFTSPC, MO(_ADJUST), KC_RALT
+                              KC_LGUI, MO(_ADJUST), TD(TD_BSPACE), KC_DELETE,     KC_ENT, KC_SFTSPC, MO(_ADJUST), KC_RALT
     ),
     [_LOWER] = LAYOUT_split_3x7_4(
         _______, KC_AT,   _______, _______,  _______, _______, _______,    OP_ARROW, _______, KC_LCBR, KC_RCBR, _______, _______, _______,
