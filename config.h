39aab9667c1ad4d156164e04e1a63b8f007d492a
David Sunshine <david@sunshines.org>
2021-08-25 14:45:12 -0400

    Tweaked tapping time and tap hold settings

--- config.h
+++ config.h
@@ -27,10 +27,11 @@
 #define USE_SERIAL_PD2
 
 #define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 150
-#define RETRO_TAPPPING
+#define TAPPING_TERM 175
+/* #define RETRO_TAPPPING */
+#define PERMISSIVE_HOLD
 
 #define UNICODE_SELECTED_MODES UC_WINC
 
-#define ONESHOT_TAP_TOGGLE 2  /* Tapping this number of times holds the key until tapped once again. */
+#define ONESHOT_TAP_TOGGLE 3  /* Tapping this number of times holds the key until tapped once again. */
 #define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */
